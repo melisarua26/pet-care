667592b02588b12e8fd23a06fd949a0acb105762
Melisa Rua <melisa.2610@hotmail.com>
2022-10-15 14:13:34 -0500

    new app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def is_navigational_format?
+    Devise.navigational_formats.include?(request_format)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :vets, path: 'vets', controllers: { sessions: "vets/sessions" }
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
