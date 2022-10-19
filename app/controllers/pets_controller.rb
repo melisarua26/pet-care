d27805ceb9a32d02fe409ad88cb4006e33da01bc
Melisa Rua <melisa.2610@hotmail.com>
2022-10-18 22:04:26 -0500

    pet views

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,44 @@
 class PetsController < ApplicationController
+
+  def index
+    @pets = Pet.all
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    redirect_to root_path unless @pet.user == current_user
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(place_params)
+    redirect_to pet_path(@pet)
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :gender, :birthdate, :specie, :breed, :photo)
+  end
+
+  def set_place
+    @pets = Pet.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :vets, path: 'vets', controllers: { sessions: "vets/sessions" }
-  devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
+  devise_for :vets, path: 'vets', controllers: { sessions: "vets/sessions", registrations: "vets/registrations" }
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations"}
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :pets
+  resources :vets
+  resources :appointments
   # Defines the root path route ("/")
   # root "articles#index"
 end
