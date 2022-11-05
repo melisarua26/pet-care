0dab85b8f286153e48aa9c20ebcb3ff502e90086
Melisa Rua <melisa.2610@hotmail.com>
2022-11-04 23:06:21 -0500

    appointments not working

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,16 @@
 class Appointment < ApplicationRecord
+  validates :start_time, :end_time, presence: true
   belongs_to :pet
   belongs_to :vet
+
+  default_scope -> { order(:start_time) }  # Our meetings will be ordered by their start_time by default
+
+  def time
+    "#{start_time.strftime('%I:%M %p')} - #{end_time.strftime('%I:%M %p')}"
+  end
+
+  def multi_days?
+    (end_time.to_date - start_time.to_date).to_i >= 1
+  end
+  
 end
